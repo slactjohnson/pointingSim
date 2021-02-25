f40bfb961291c0076b7914bf1d9685f34383b30f
slactjohnson <tjohnson@slac.stanford.edu>
2021-02-24 19:21:09 -0800

    Initial commit.

---
+++ pointingSimScreen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Setup the common directory env variables
+if [ -e      /reg/g/pcds/pyps/config/common_dirs.sh ]; then
+    source   /reg/g/pcds/pyps/config/common_dirs.sh
+elif [ -e    /afs/slac/g/pcds/pyps/config/common_dirs.sh ]; then
+    source   /afs/slac/g/pcds/pyps/config/common_dirs.sh
+fi
+
+# Setup edm environment
+if [ -f    ${SETUP_SITE_TOP}/epicsenv-cur.sh ]; then
+    source ${SETUP_SITE_TOP}/epicsenv-cur.sh
+fi
+
+export CAM=LAS:TEST:NF
+
+edm -x -eolc \
+    -m "CAM=${CAM}" \
+    ./pointingSim.edl &
+
